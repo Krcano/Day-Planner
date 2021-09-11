7dcc26031163823a3728bd929e677df6d2250b86
Kelly R Cano <krcano2001@gmail.com>
2021-09-11 15:49:14 -0800

    added current date and a js file

---
+++ script.js
@@ -0,0 +1,10 @@
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY, h:mm:ss a"));
+
+//To Do list
+/* 1. Create standard business  time slots ~9am-5pm
+        a.Must be color-coded to show events on past present and future dates
+   2. When timeblock is clicked can insert text
+        a. Needs a save butto to save event
+   3. Must savec to local storage even after refreshing the page
+  */
